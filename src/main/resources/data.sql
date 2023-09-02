6bb835a4c723fc1828e29511381301e067bfbcd9
7zllco <7zllco99@gmail.com>
2023-09-02 13:02:48 +0900

    9장 CRUD와 SQL 쿼리 종합

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
-INSERT INTO article(id, title, content) VALUES(1, '어제', '잘 쉬었다');
-INSERT INTO article(id, title, content) VALUES(2, '오늘', '열심히 했다');
-INSERT INTO article(id, title, content) VALUES(3, '내일', '열심히 한다');
+INSERT INTO article(title, content) VALUES('어제', '잘 쉬었다');
+INSERT INTO article(title, content) VALUES('오늘', '열심히 했다');
+INSERT INTO article(title, content) VALUES('내일', '열심히 한다');
