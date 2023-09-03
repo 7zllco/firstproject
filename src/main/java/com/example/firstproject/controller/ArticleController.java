72c757461aa26c0701cc7772c9e6d9299357050b
7zllco <7zllco99@gmail.com>
2023-09-03 17:36:58 +0900

    16장 웹 페이지에서 댓글 목록 보기

--- src/main/java/com/example/firstproject/controller/ArticleController.java
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -1,8 +1,11 @@
 package com.example.firstproject.controller;
 
 import com.example.firstproject.dto.ArticleForm;
+import com.example.firstproject.dto.CommentForm;
 import com.example.firstproject.entity.Article;
+import com.example.firstproject.entity.Comment;
 import com.example.firstproject.repository.ArticleRepository;
+import com.example.firstproject.service.CommentService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -13,6 +16,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.ArrayList;
+import java.util.List;
 
 @Slf4j
 @Controller
@@ -20,6 +24,8 @@ public class ArticleController {
 
     @Autowired
     private ArticleRepository articleRepository;
+    @Autowired
+    private CommentService commentService;
 
     @GetMapping("/articles/new")
     public String newArticle(Model model){
@@ -49,8 +55,10 @@ public class ArticleController {
         log.info("id = " + id);
         //1. id를 조회해 데이터 가져오기
         Article articleEntity = articleRepository.findById(id).orElse(null); //Optional<Article> articleEntity = articleRepository.findById(id); 도 가능
+        List<CommentForm> commentDtos = commentService.comments(id);
         //2. 가져온 데이터를 모델에 등록하기
         model.addAttribute("article", articleEntity);
+        model.addAttribute("commentDtos", commentDtos);
         //3. 사용자에게 데이터 보여주기 위해 뷰 페이지 반환하기
         return "articles/show";
     }
